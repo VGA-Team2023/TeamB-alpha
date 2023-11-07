0ba4955cfa364967c4398d2a3069f157521a05d4
Harumi Maruoka <106292983+HarumiMaruoka@users.noreply.github.com>
2023-11-07 18:06:43 +0900

    [add] Craft Resource Manager を作成。

---
+++ Assets/Battle/Craft/CraftResourceSlider.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace TeamB_TD
+{
+    namespace Battle
+    {
+        namespace Craft
+        {
+            public class CraftResourceSlider : MonoBehaviour
+            {
+                [SerializeField]
+                private CraftResourceManager _resourceManager;
+                [SerializeField]
+                private Slider _slider;
+
+                private void Start()
+                {
+                    _slider.maxValue = _resourceManager.MaxResource;
+                    _slider.minValue = 0f;
+
+                    ApplyValue(_resourceManager.CurrentResource);
+
+                    _resourceManager.OnResourceChanged += ApplyValue;
+                }
+
+                private void ApplyValue(float value)
+                {
+                    _slider.value = value;
+                }
+            }
+        }
+    }
+}
