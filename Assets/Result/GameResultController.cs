9b83f1e7c8bfd03fea8afbf22620d85ebbce780e
RyuOomiya <106770603+RyuOomiya@users.noreply.github.com>
2023-11-15 14:16:02 +0900

    [add] namespace追加

--- Assets/Result/GameResultController.cs
+++ Assets/Result/GameResultController.cs
@@ -4,25 +4,33 @@ using TeamB_TD.Battle.Unit.Enemy;
 using TeamB_TD.Tower;
 using UnityEngine;
 
-public class GameResultController : MonoBehaviour
+namespace TeamB_TD
 {
-    [SerializeField] GameResultViewer _gameResultViewer;
-    [SerializeField] TowerController _towerController;
-
-    public void ResultScoreSet()
+    namespace Result
     {
-        EnableResultPanel();
-        _gameResultViewer.DeadEnemyCountSet(EnemyCounter.Current.DeadEnemyCount);
-        _gameResultViewer.TowerHPSet(_towerController.Life);
-    }
+        public class GameResultController : MonoBehaviour
+        {
+            [SerializeField] GameResultViewer _gameResultViewer;
+            [SerializeField] TowerController _towerController;
 
-    public void EnableResultPanel()
-    {
-        this.gameObject.SetActive(true);
-    }
+            public void ResultScoreSet()
+            {
+                EnableResultPanel();
+                _gameResultViewer.DeadEnemyCountSet(EnemyCounter.Current.DeadEnemyCount);
+                _gameResultViewer.TowerHPSet(_towerController.Life);
+            }
 
-    public void DisableResultPanel() 
-    {
-        this .gameObject.SetActive(false); 
+            public void EnableResultPanel()
+            {
+                this.gameObject.SetActive(true);
+            }
+
+            public void DisableResultPanel()
+            {
+                this.gameObject.SetActive(false);
+            }
+        }
     }
 }
+
+
--- Assets/Result/GameResultViewer.cs
+++ Assets/Result/GameResultViewer.cs
@@ -4,18 +4,26 @@ using UnityEngine;
 using UnityEngine.Events;
 using UnityEngine.UI;
 
-public class GameResultViewer : MonoBehaviour
+namespace TeamB_TD
 {
-    [SerializeField] private Text _deadEnemyCount;
-    [SerializeField] private Text _towerHP;
-
-    public void DeadEnemyCountSet(int value)
+    namespace Result
     {
-        _deadEnemyCount.text = $"殲滅数 : {value.ToString()}";
-    }
+        public class GameResultViewer : MonoBehaviour
+        {
+            [SerializeField] private Text _deadEnemyCount;
+            [SerializeField] private Text _towerHP;
 
-    public void TowerHPSet(int value)
-    {
-        _towerHP.text = $"タワーHP : {value}";
+            public void DeadEnemyCountSet(int value)
+            {
+                _deadEnemyCount.text = $"殲滅数 : {value.ToString()}";
+            }
+
+            public void TowerHPSet(int value)
+            {
+                _towerHP.text = $"タワーHP : {value}";
+            }
+        }
     }
 }
+
+
