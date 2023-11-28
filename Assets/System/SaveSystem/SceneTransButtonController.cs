80a3c19544f0eb3519be6b68666c16749752598c
SHOW\shoei <shoeish0326@gmail.com>
2023-11-28 18:49:03 +0900

    セーブロード系の機能追加。タイトルシーンからステージセレクトシーンの遷移のデバッグ用機能追加

---
+++ Assets/System/SaveSystem/SceneTransButtonController.cs
@@ -0,0 +1,39 @@
+﻿using UnityEngine;
+using UnityEngine.EventSystems;
+using TeamB_TD.UI;
+
+namespace TeamB_TD
+{ 
+    namespace SaveData
+    {
+        public class SceneTransButtonController : MonoBehaviour, IPointerClickHandler
+        {
+            [SerializeField] SceneName _nextScene;
+
+            bool _isActive = true;
+            public bool Active { set { _isActive = value; } }
+
+            public void OnPointerClick(PointerEventData pointerEventData)
+            {
+                if (_isActive)
+                {
+                    SceneTransition.instance.SceneTrans(_nextScene.ToString());
+                }
+            }
+            public void BeUntouchable()
+            {
+                this.transform.GetChild(1).gameObject.SetActive(true);
+            }
+        }
+        public enum SceneName
+        {
+            TitleScene,
+            StageSelect,
+            Tutrial,
+            Stage1,
+            Stage2,
+            Stage3,
+        }
+    }
+}
+
---
+++ Assets/System/SaveSystem/StageSelectUIController.cs
@@ -0,0 +1,35 @@
+﻿using System.IO;
+using UnityEngine;
+
+namespace TeamB_TD
+{
+    namespace SaveData
+    {
+        public class StageSelectUIController : MonoBehaviour
+        {
+            [SerializeField] SceneTransButtonController[] _buttons;
+            string _path;
+            SaveData _pData;
+
+            private void Start()
+            {
+                _path = Application.dataPath + "/SaveData/" + DataManager.GetPDataFileName();
+                StreamReader rd = new StreamReader(_path);
+                //string json = rd.ReadToEnd();
+                string json = DataManager.GetPDataJson();
+                //rd.Close();
+                Debug.Log(json);
+                _pData = JsonUtility.FromJson<SaveData>(json);
+                for (int i = 0; i < _buttons.Length - 1; i++)
+                {
+                    if (!_pData._isClear[i])
+                    {
+                        _buttons[i + 1].Active = false;
+                        _buttons[i + 1].BeUntouchable();
+                    }
+                }
+            }
+        }
+    }
+}
+
