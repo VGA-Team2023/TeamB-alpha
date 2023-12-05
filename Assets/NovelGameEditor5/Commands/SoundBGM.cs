3e0e4642adbc65fc780413442dd59161d6949797
MizutamariShutarou <shutaro0415@outlook.jp>
2023-12-05 20:33:35 +0900

    [add] シナリオコマンドを追加。音を鳴らす処理作成。

---
+++ Assets/NovelGameEditor5/Commands/SoundBGM.cs
@@ -0,0 +1,29 @@
+﻿// 日本語対応
+using Cysharp.Threading.Tasks;
+using System.Threading;
+
+namespace TeamB_TD
+{
+    namespace NovelGameEditor5.Commands
+    {
+        public class SoundBGM : ICommand
+        {
+            private string _cueSheetName;
+            private string _cueName;
+            private float _volume;
+
+            public SoundBGM(string[] commandArgs)
+            {
+                _cueSheetName = commandArgs[0];
+                _cueName = commandArgs[1];
+                _volume = float.Parse(commandArgs[2]);
+            }
+
+            public async UniTask RunCommand(CancellationToken token = default)
+            {
+                var audioManager = CriAudioManager.Instance;
+                await audioManager.SoundBGM(_cueSheetName, _cueName, _volume);
+            }
+        }
+    }
+}
---
+++ Assets/NovelGameEditor5/Commands/SoundVoice.cs
@@ -0,0 +1,29 @@
+﻿// 日本語対応
+using Cysharp.Threading.Tasks;
+using System.Threading;
+
+namespace TeamB_TD
+{
+    namespace NovelGameEditor5.Commands
+    {
+        public class SoundVoice : ICommand
+        {
+            private string _cueSheetName;
+            private string _cueName;
+            private float _volume;
+
+            public SoundVoice(string[] commandArgs)
+            {
+                _cueSheetName = commandArgs[0];
+                _cueName = commandArgs[1];
+                _volume = float.Parse(commandArgs[2]);
+            }
+
+            public async UniTask RunCommand(CancellationToken token = default)
+            {
+                var audioManager = CriAudioManager.Instance;
+                await audioManager.SoundVoice(_cueSheetName, _cueName, _volume);
+            }
+        }
+    }
+}
