f8b953940c3c50ae4d3e3f0192991c1811f0e9f2
tatsupiko <identity2022@outlook.jp>
2023-12-05 19:52:36 +0900

    [update] 各種enumの値を編集した。

--- Assets/Battle/Stage/Core/StageCellStatus.cs
+++ Assets/Battle/Stage/Core/StageCellStatus.cs
@@ -10,8 +10,9 @@ namespace TeamB_TD
             [Flags]
             public enum StageCellStatus
             {
+                None = 0, // デフォルト
                 Movable = 1, // 敵ユニットが移動可能かどうか表現する。
-                UnitPlacable = 2, // 味方ユニットが配置可能かどうか表現する。
+                UnitPlaceable = 2, // 味方ユニットが配置可能かどうか表現する。
             }
         }
     }
