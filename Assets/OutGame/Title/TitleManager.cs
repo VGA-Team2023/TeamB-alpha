723c25ed292dabebecad43d2b854a83fa204be95
MizutamariShutarou <shutaro0415@outlook.jp>
2023-12-05 16:11:13 +0900

    [add] TitleManager追加

---
+++ Assets/OutGame/Title/TitleManager.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using TeamB_TD.SaveData;
+using UnityEngine;
+
+public class TitleManager : MonoBehaviour
+{
+    private void Awake()
+    {
+        DataManager.Instance.Initialize();
+    }
+}
