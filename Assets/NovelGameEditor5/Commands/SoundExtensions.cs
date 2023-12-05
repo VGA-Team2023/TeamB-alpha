e23d93a3183175a2c0e9f1b849a2d4e4f28adcf4
MizutamariShutarou <shutaro0415@outlook.jp>
2023-12-05 22:54:20 +0900

    [add] 追加し忘れ

---
+++ Assets/NovelGameEditor5/Commands/SoundExtensions.cs
@@ -0,0 +1,32 @@
+﻿// 日本語対応
+using Cysharp.Threading.Tasks;
+using System.Threading;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace TeamB_TD
+{
+    namespace NovelGameEditor5
+    {
+        public static class SoundExtensions
+        {
+            public static async UniTask SoundBGM(this CriAudioManager audioManager, string cueSheetName, string cueName, float volume, CancellationToken token = default)
+            {
+                audioManager.BGM.Play(cueSheetName, cueName, volume);
+                await UniTask.CompletedTask;
+            }
+
+            public static async UniTask SoundSE(this CriAudioManager audioManager, string cueSheetName, string cueName, float volume, CancellationToken token = default)
+            {
+                audioManager.SE.Play(cueSheetName, cueName, volume);
+                await UniTask.CompletedTask;
+            }
+
+            public static async UniTask SoundVoice(this CriAudioManager audioManager, string cueSheetName, string cueName, float volume, CancellationToken token = default)
+            {
+                audioManager.SE.Play(cueSheetName, cueName, volume);
+                await UniTask.CompletedTask;
+            }
+        }
+    }
+}
