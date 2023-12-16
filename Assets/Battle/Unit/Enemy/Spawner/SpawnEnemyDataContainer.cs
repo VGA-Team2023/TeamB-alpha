27748c0f686704fb533eb066449bf61bda11c0c3
Harumi Maruoka <106292983+HarumiMaruoka@users.noreply.github.com>
2023-12-16 11:34:21 +0900

    [update] Stage2のSpawner Position Dataを作成,割り当て。ついでにEnemy Spawn Dataも割り当てました。

--- Assets/Battle/Unit/Enemy/Spawner/SpawnEnemyDataContainer.cs
+++ Assets/Battle/Unit/Enemy/Spawner/SpawnEnemyDataContainer.cs
@@ -23,19 +23,34 @@ namespace TeamB_TD
                     {
                         new SpawnEnemyData[] // 一スポナー分のデータ
                         {
-                            //TODO:α用に引数いじったのでリファクタする
                             new SpawnEnemyData(0,4f) , // 一体分のデータ（例: EnemyID 0をバトル開始後 1秒後に生成する。）
                             new SpawnEnemyData(1,8f) , // （例: EnemyID 1をバトル開始後 2秒後に生成する。）
                             new SpawnEnemyData(2,10f) , // （例: EnemyID 2をバトル開始後 3秒後に生成する。）
                             new SpawnEnemyData(3,12f) , // （例: EnemyID 3をバトル開始後 4秒後に生成する。）
                         },
+
+                         // ID 1 (Dummy data)
+                        new SpawnEnemyData[] { },
+                         // ID 2 (Dummy data)
+                        new SpawnEnemyData[] { },
+                         // ID 3 (Dummy data)
+                        new SpawnEnemyData[] { },
+                         // ID 4 (Dummy data)
+                        new SpawnEnemyData[] { },
+                         // ID 5 (Dummy data)
+                        new SpawnEnemyData[] { },
+                         // ID 6 (Dummy data)
+                        new SpawnEnemyData[] { },
+                         // ID 7 (Dummy data)
+                        new SpawnEnemyData[] { },
+                         // ID 8 (Dummy data)
+                        new SpawnEnemyData[] { },
                     };
 
                     public static SpawnEnemyData[][] SpawnData => _spawnData;
 
                     public SpawnEnemyData[] GetSpawnData(int id)
                     {
-
                         if (!ArrayUtility.IsIndexInRange(_spawnData, id))
                             return null;
                         // throw new ArgumentOutOfRangeException(nameof(id));
