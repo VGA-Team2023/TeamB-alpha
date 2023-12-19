5e830f297eb550135a8ac8052e07f6791025b331
SHOW\shoei <shoeish0326@gmail.com>
2023-12-20 04:15:51 +0900

    コメントの削除

--- Assets/System/SaveSystem/StageSelectUIController.cs
+++ Assets/System/SaveSystem/StageSelectUIController.cs
@@ -10,9 +10,7 @@ namespace TeamB_TD
         public class StageSelectUIController : MonoBehaviour
         {
             [SerializeField] SceneTransButtonController[] _buttons;
-            string _path;
-            SaveData _pData;
-
+            
             private void Start()
             {
                 SaveData instantData = DataManager.Instance.Load();
@@ -23,22 +21,7 @@ namespace TeamB_TD
                         _buttons[i + 1].Active = false;
                         _buttons[i + 1].BeUntouchable();
                     }
-                }
-                //_path = Application.streamingAssetsPath + "/SaveData/" + DataManager.GetPDataFileName();
-                //StreamReader rd = new StreamReader(_path);
-                ////string json = rd.ReadToEnd();
-                //string json = DataManager.GetPDataJson();
-                ////rd.Close();
-                //Debug.Log(json);
-                //_pData = JsonUtility.FromJson<SaveData>(json);
-                //for (int i = 0; i < _buttons.Length - 1; i++)
-                //{
-                //    if (!_pData._isClear[i])
-                //    {
-                //        _buttons[i + 1].Active = false;
-                //        _buttons[i + 1].BeUntouchable();
-                //    }
-                //}
+                }                
             }
         }
     }
