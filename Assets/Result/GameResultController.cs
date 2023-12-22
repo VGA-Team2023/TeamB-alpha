2677ac53ec77470b4ff74f16432d9759c5d2ce24
ryuoomiya <oomiyaryu@gmail.com>
2023-12-23 06:18:41 +0900

    [update] オブジェクトとアニメーションの調整

--- Assets/Result/GameResultController.cs
+++ Assets/Result/GameResultController.cs
@@ -1,10 +1,11 @@
 ﻿using System.Collections;
-using TeamB_TD.Battle.Unit.Enemy;
 using TeamB_TD.Battle.Tower;
 using TeamB_TD.UI;
 using TeamB_TD.SaveData;
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 namespace TeamB_TD
 {
@@ -16,6 +17,7 @@ namespace TeamB_TD
             [SerializeField] GameResultViewer _gameResultViewer;
             [SerializeField] GameClearAnimation _gameClearAnimation;
             [SerializeField] TowerController _towerController;
+            [SerializeField] ResultSoundManager _soundManager;
 
             public GameResultViewer GameResultViewer => _gameResultViewer;
 
@@ -27,18 +29,19 @@ namespace TeamB_TD
 
             public IEnumerator GameClearResultSet()
             {
+                _soundManager.ClearBGMPlay();
                 _gameResultViewer.TowerHPSet(_towerController.CurrentLife);
                 _gameResultViewer.GameClearPanelChangeActive(true);
-                yield return _gameClearAnimation.GameClearTextAnimation();
+                yield return _gameClearAnimation.GameClearImageAnimation();
                 _gameResultViewer.ScorePanelChangeActive(true);
                 StartCoroutine(_gameResultViewer.ScorePanelSet());
                 yield return _gameClearAnimation.
                     StarImageAnimationStart(StarCountCalculation(_towerController.CurrentLife));
                 yield return _gameClearAnimation.
                     CharaImageAnimation(_gameResultViewer.CharaImage, _gameResultViewer.DispImgPos);
-                _gameResultViewer.ResultClickPanelChangeActive(true);
-
                 SaveData.SaveData instantData = DataManager.Instance.Load();
+                _soundManager.ClearVoiceSelect(instantData._favoriteUnitId,_towerController.CurrentLife);
+                _gameResultViewer.ResultClickPanelChangeActive(true);
                 instantData._isClear[_stageNum] = true;
                 DataManager.Instance.Save(instantData);
             }
@@ -53,9 +56,9 @@ namespace TeamB_TD
                 _gameResultViewer.GameOverPanelChangeActive(true);
             }
 
-            public void TransitionButton()
+            public void TransitionButton(string name)
             {
-                SceneTransition.instance.SceneTrans("Scenario 1");
+                SceneTransition.instance.SceneTrans(name);
             }
         }
     }
