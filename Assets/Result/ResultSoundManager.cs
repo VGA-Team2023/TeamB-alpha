b3824ae52d00ed4118f28174d75d046af909b9cb
ryuoomiya <oomiyaryu@gmail.com>
2023-12-23 13:09:42 +0900

    [add] ResultSoundManager上げ忘れ

---
+++ Assets/Result/ResultSoundManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResultSoundManager : MonoBehaviour
+{
+    public void ClearBGMPlay()
+    {
+        CriAudioManager.Instance.BGM.Play("BGM", "BGM_002_result");
+    }
+
+    public void ClearVoiceSelect(int charaID, int starCount)
+    {
+        int voiceID = 0;
+        if (starCount == 5) voiceID = 1;
+        else if (starCount == 4) voiceID = 2;
+        else if (starCount == 3) voiceID = 3;
+        else if (starCount == 2) voiceID = 4;
+        else if (starCount == 1) voiceID = 5;
+        if (voiceID != 0)
+        {
+            CriAudioManager.Instance.BGM.Play($"unit{charaID}", $"VOICE09_result{charaID}_{voiceID}");
+        }
+    }
+}
